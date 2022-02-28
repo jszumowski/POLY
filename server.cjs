7b4a4e316c6dcded1330e113eaf38a4062c6074a
jszumowski <85529714+jszumowski@users.noreply.github.com>
2022-02-28 15:44:24 -0500

    updated grading mechanism and eliminated postlight API

--- server.cjs
+++ server.cjs
@@ -1,3 +1,12 @@
+/*'use strict';
+var http = require('http');
+var port = process.env.PORT || 1337;
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);*/
+
 const http = require('http');
 const express = require('express');
 const path = require('path');
